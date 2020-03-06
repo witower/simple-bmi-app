9f153c3a4b7ac4510dbbc6c2790474c0f6d89767
witower <25256638+witower@users.noreply.github.com>
2020-03-07 00:47:49 +0100

    BmiCalc 0.001

---
+++ app/src/main/java/com/example/bmiCalculator/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.bmiCalculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.bmiCalculator.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Witaj PJATK! Hello World", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void calculateBmi(View view) {
+        Intent intent = new Intent(this, BmiResultActivity.class);
+        EditText heightInput = findViewById(R.id.heightInput);
+        EditText weightInput = findViewById(R.id.weightInput);
+        Integer height = Integer.valueOf(heightInput.getText().toString());
+        Integer weight = Integer.valueOf(weightInput.getText().toString());
+        Float bmi = (float) weight/height/height*10000;
+
+        intent.putExtra(EXTRA_MESSAGE, bmi.toString());
+        startActivity(intent);
+    }
+}
